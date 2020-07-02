e6d30c0ef205d6627bf5ee4517cb62489f1bd75e
Margarida <a78376@alunos.uminho.pt>
2020-07-02 15:42:54 +0100

    Registar

---
+++ app/Publication.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Publication extends Model {
+
+    protected $table = 'publication';
+    protected $fillable = [
+        'message',
+        'user_id'
+    ];
+  
+
+}
