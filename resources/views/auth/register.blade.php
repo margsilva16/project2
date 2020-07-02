d4ebd1deab77509e18170031001ad892346d6ce3
Margarida <a78376@alunos.uminho.pt>
2020-07-02 23:54:42 +0100

    App

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,27 +18,27 @@
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('nome')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
-                        
-                       <div class="form-group row">
+
+                        <div class="form-group row">
                             <label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Apelido') }}</label>
 
                             <div class="col-md-6">
                                 <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ old('surname') }}" required autofocus>
 
                                 @error('surname')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
-                        
+
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
@@ -47,9 +47,9 @@
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -61,9 +61,9 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
--- routes/web.php
+++ routes/web.php
@@ -30,18 +30,6 @@ Route::delete('/home/{idPublication}', 'PublicationController@destroy');
 
 Route::get('/list/{idPublication}', 'PublicationController@destroy');
 
-
-
-
-
-//
-//Route::get('/list', ['uses' => 'publicacoesController@index']);
-//
-//Route::resource('publications', 'publicacoesController');
-//
-//Route::get('/list', 'publicacoesController@index');
-//
-//Route::get('/info', 'Controller@index');
 Route::get('/password', function() {
         return view('auth.passwords.reset');
 });
@@ -50,21 +38,5 @@ Route::get('/change-password', 'ChangePasswordController@index');
 Route::post('change-password', 'ChangePasswordController@store')->name('change.password');
 
 Route::get('/auth/passwords/reset', 'publicacoesController@index');
-//
-//Route::delete('/home/{idPublication}', 'publicacoesController@destroy');
-//
-Route::get('/reset', 'ResetPasswordController@showChangePasswordForm');
-//
-//Auth::routes();
-//
-//Route::get('/home', 'HomeController@index')->name('home');
-//
-//Auth::routes();
-//
-//Route::get('/home', 'HomeController@index')->name('home');
-//
-//Auth::routes();
-//
-//Route::get('/home', 'HomeController@index')->name('home');
-//
-//
+
+//Route::get('/reset', 'ResetPasswordController@showChangePasswordForm');
